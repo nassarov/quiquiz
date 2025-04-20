c876470232ff2f022de2fcbc7fd75c21022bd31a
Ali Nassar <aliinassar8@gmail.com>
2025-04-20 21:21:39 +0200

    Defining elements to be used

---
+++ js/auth.js
@@ -0,0 +1,46 @@
+const log = document.querySelector(".log");
+const regForm = document.querySelector(".reg-form");
+console.log(regForm);
+let users = JSON.parse(localStorage.getItem("users"));
+console.log(users);
+regForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let confirm = document.getElementById("confirm-password").value;
+  if (password !== confirm) {
+    console.log(`${password} not equal ${confirm}`);
+    let confirmLabel = document.querySelector(".confirm");
+    confirmLabel.classList.add("error");
+    confirmLabel.textContent = "Make sure confirmation is same as password!";
+
+    setTimeout(() => {
+      confirmLabel.classList.remove("error");
+      confirmLabel.textContent = "Confirm Your Password";
+    }, 5000);
+  } else {
+    let exists = false;
+    for (let user of users) {
+      if (user.email === email) {
+        exists = true;
+        break;
+      }
+    }
+    if (exists) {
+      alert(`User with email ${email} already exists!`);
+      console.log(`Already exist ${email}`);
+    } else {
+      users.push({ email, password, role: "user" });
+      localStorage.setItem("users", JSON.stringify(users));
+      console.log(`Registration successful for ${email}`);
+      alert("Registration successful! It's time to Login...");
+      window.location.replace("../login.html");
+    }
+    console.log(`status:${exists}`);
+  }
+  console.log(localStorage.getItem("users"));
+});
+console.log(log);
+log.addEventListener("click", () => {
+  window.location.replace("../login.html");
+});
