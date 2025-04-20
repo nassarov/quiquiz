42d4242b178463e35dea412dcf9cc14f2ffcd29d
Ali Nassar <aliinassar8@gmail.com>
2025-04-20 21:20:29 +0200

    Checking for users in localStorage if found or not if not create a new one with a reserverd credentails for the admin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const logButton = document.getElementById("log-reg");
 console.log(logButton);
 logButton.addEventListener("click", () => {
-  window.location.href = "../login.html";
+  window.location.href = "../register.html";
 });
+
+// check first if users exist
+if (!localStorage.getItem("users")) {
+  let users = [
+    { email: "admin@quiz.com", password: "admin123", role: "admin" },
+  ];
+  localStorage.setItem("users", JSON.stringify(users));
+}
