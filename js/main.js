2d23b794a310c223fcb5956c4566d5c744162d14
Ali Nassar <aliinassar8@gmail.com>
2025-04-21 02:12:17 +0200

    Adding some quiz topics and questions

--- js/main.js
+++ js/main.js
@@ -11,3 +11,86 @@ if (!localStorage.getItem("users")) {
   ];
   localStorage.setItem("users", JSON.stringify(users));
 }
+
+if (!localStorage.getItem("quizzes")) {
+  let quizzes = [
+    {
+      id: 1,
+      title: "JavaScript Basics",
+      score: 0,
+      questions: [
+        {
+          question: "Which company developed JavaScript?",
+          options: ["Microsoft", "Netscape", "Google", "Apple"],
+          answer: "Netscape",
+        },
+        {
+          question: "Which symbol is used for comments in JavaScript?",
+          options: ["//", "<!-- -->", "/* */", "#"],
+          answer: "//",
+        },
+        {
+          question: "What data type is `null` in JavaScript?",
+          options: ["Object", "Null", "Undefined", "Boolean"],
+          answer: "Object",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "HTML & CSS",
+      score: 0,
+      questions: [
+        {
+          question: "What does HTML stand for?",
+          options: [
+            "HyperText Markup Language",
+            "HighText Markdown Language",
+            "Hyperloop Machine Language",
+            "Hyper Transfer Markup Language",
+          ],
+          answer: "HyperText Markup Language",
+        },
+        {
+          question: "Which property changes text color in CSS?",
+          options: [
+            "font-style",
+            "background-color",
+            "color",
+            "text-decoration",
+          ],
+          answer: "color",
+        },
+        {
+          question: "Which HTML tag is used for creating a hyperlink?",
+          options: ["<a>", "<link>", "<href>", "<hyperlink>"],
+          answer: "<a>",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "General Knowledge",
+      score: 0,
+      questions: [
+        {
+          question: "What is the capital city of France?",
+          options: ["London", "Berlin", "Paris", "Madrid"],
+          answer: "Paris",
+        },
+        {
+          question: "Which planet is known as the Red Planet?",
+          options: ["Earth", "Saturn", "Mars", "Jupiter"],
+          answer: "Mars",
+        },
+        {
+          question: "What is the largest ocean on Earth?",
+          options: ["Atlantic", "Indian", "Arctic", "Pacific"],
+          answer: "Pacific",
+        },
+      ],
+    },
+  ];
+
+  localStorage.setItem("quizzes", JSON.stringify(quizzes));
+}
