35c5abe78cf92b774b6c1c5755a9afe0e6fd30c2
Ali Nassar <aliinassar8@gmail.com>
2025-04-21 15:57:08 +0200

    Defining DOM elements to be used

---
+++ js/quiz.js
@@ -0,0 +1,4 @@
+let selectedId = localStorage.getItem("selectedQuiz");
+const container = document.getElementById("container");
+const form = document.getElementById("quiz-form");
+const submit = document.getElementById("log-reg");
