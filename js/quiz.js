d386baf878d5e634fd36fc84b27b67dfd4965192
Ali Nassar <aliinassar8@gmail.com>
2025-04-21 15:57:48 +0200

    Preventing default form submit action

--- js/quiz.js
+++ js/quiz.js
@@ -2,3 +2,6 @@ let selectedId = localStorage.getItem("selectedQuiz");
 const container = document.getElementById("container");
 const form = document.getElementById("quiz-form");
 const submit = document.getElementById("log-reg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
