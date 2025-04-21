caec0ab850f36627f75f7c49cede7c021dcdee47
Ali Nassar <aliinassar8@gmail.com>
2025-04-21 22:19:17 +0200

    Defining elements needed

---
+++ js/dashboard.js
@@ -0,0 +1,13 @@
+const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+
+// Role check: redirect if not admin
+if (!loggedUser) {
+  window.location.replace("../login.html");
+} else if (loggedUser.role !== "admin") {
+  window.location.replace("../home.html");
+} else {
+  console.log("Welcome, Admin!");
+}
+
+const container = document.getElementById("users-table");
+const refreshButton = document.getElementById("refresh-btn");
