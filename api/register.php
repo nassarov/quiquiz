205ae028b94ff38ad49a0506f74b5e50425833d0
Ali Nassar <aliinassar8@gmail.com>
2025-04-26 17:41:05 +0300

    Selecting from users table if user with unique email aleardy exist

--- api/register.php
+++ api/register.php
@@ -5,6 +5,11 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
     $data = json_decode($json,true);
     $email = filter_var($data['email']);
     $password = filter_var($data['password']);
-    
+    if(!empty($email) && !empty($password)){
+        // if already exist
+        $check = "SELECT * FROM users WHERE email = '$email'";
+        $result = mysqli_query(mysql: $conn, query: $check);
+       
+}
 }
 ?>
