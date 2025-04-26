b4acdf0ac8dda3e21d2c64d04f55809f2551b7c8
Ali Nassar <aliinassar8@gmail.com>
2025-04-26 23:33:58 +0300

    Storing password hashed instead of plain text

--- api/register.php
+++ api/register.php
@@ -3,8 +3,8 @@ require 'conn.php';
 if($_SERVER["REQUEST_METHOD"]== "POST"){
     $json = file_get_contents('php://input');
     $data = json_decode($json,true);
-    $email = filter_var($data['email']);
-    $password = filter_var($data['password']);
+    $email = filter_var($data['email'],FILTER_SANITIZE_EMAIL);
+    $password = filter_var($data['password'],FILTER_SANITIZE_SPECIAL_CHARS);
     if(!empty($email) && !empty($password)){
         // if already exist
         $check = "SELECT * FROM users WHERE email = '$email'";
@@ -13,7 +13,7 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
             echo json_encode(["status"=>"error","message"=>"Email already exist!"]);
         }
         else{
-            $sql = "INSERT INTO users(email,password) VALUES ('$email','$password')";
+            $sql = "INSERT INTO users(email,password) VALUES ('$email','" . password_hash($password, PASSWORD_DEFAULT) . "')";
             try{
                 mysqli_query($conn,$sql);
                 echo json_encode(["status"=> "success","message"=> "Account created go and Login"]);
