8f977d6023cdd3a73960833785acf411b2ff7026
Ali Nassar <aliinassar8@gmail.com>
2025-04-26 17:39:29 +0300

    Checking on request and declaring vars with data to be used sent from js file

---
+++ api/register.php
@@ -0,0 +1,10 @@
+<?php
+require 'conn.php';
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $json = file_get_contents('php://input');
+    $data = json_decode($json,true);
+    $email = filter_var($data['email']);
+    $password = filter_var($data['password']);
+    
+}
+?>
