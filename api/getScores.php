57a42e988a96c10d99d9046aff77a50233881a96
Ali Nassar <aliinassar8@gmail.com>
2025-04-27 20:45:41 +0300

    Getting scores form results

--- api/getScores.php
+++ api/getScores.php
@@ -8,12 +8,10 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $stmt->bind_param("i",$userId);
     $stmt->execute();
     $result = $stmt->get_result();
-
     $results = [];
     while ($row = $result->fetch_assoc()) {
         $results[] = $row;
     }
-
-    echo json_encode(["status" => "success", "results" => $results]);
+    echo json_encode(["results" => $results]);
 }
 ?>
