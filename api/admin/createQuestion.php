5888f214d7061dd8cc07bc83bf2bd16dd127aa50
Ali Nassar <aliinassar8@gmail.com>
2025-04-27 23:31:45 +0300

    Creating Quiz

---
+++ api/admin/createQuestion.php
@@ -0,0 +1,14 @@
+<?php
+require '../conn.php';
+header('Content-Type: application/json');
+$quizId = $_POST['quizId'];
+$value = $_POST['value'];
+
+$sql = "INSERT INTO questions (quiz_id, value) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("is", $quizId, $value);
+$stmt->execute();
+    
+$stmt->close();
+$conn->close();
+?>
