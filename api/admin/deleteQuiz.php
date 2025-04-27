1cf024cf00ad17add04894c152d354c1e8ec13e0
Ali Nassar <aliinassar8@gmail.com>
2025-04-27 23:44:59 +0300

    Removing extra code

--- api/admin/deleteQuiz.php
+++ api/admin/deleteQuiz.php
@@ -3,10 +3,6 @@ require '../conn.php';
 header('Content-Type: application/json');
 
 $quizId = $_POST['quizId'];
-$sql = "DELETE FROM questions WHERE quiz_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $quizId);
-$stmt->execute(); // don't check here
 $sql = "DELETE FROM quizzes WHERE id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $quizId);
