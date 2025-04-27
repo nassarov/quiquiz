8fcdc81b6563acdc995818adede06e76f5bf5501
Ali Nassar <aliinassar8@gmail.com>
2025-04-27 23:31:15 +0300

    Deleting Quiz

---
+++ api/admin/deleteQuiz.php
@@ -0,0 +1,16 @@
+<?php
+require '../conn.php';
+header('Content-Type: application/json');
+
+$quizId = $_POST['quizId'];
+$sql = "DELETE FROM questions WHERE quiz_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $quizId);
+$stmt->execute(); // don't check here
+$sql = "DELETE FROM quizzes WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $quizId);
+$stmt->execute();
+$stmt->close();
+$conn->close();
+?>
